c87c6128f8c1d61ac49ae5bc402143288d192860
taketdev <maxleyh17@proton.me>
2025-03-14 13:30:03 +0100

    updated cart.js

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,8 +2,3 @@ function toggleCart(){
     const cart = document.getElementById("cart");
     cart.classList.toggle("d_none");
 }
-
-document.getElementById("cart_btn").addEventListener("click", function() {
-    this.classList.add("clicked");
-    setTimeout(() => this.classList.remove("clicked"), 300);
-});

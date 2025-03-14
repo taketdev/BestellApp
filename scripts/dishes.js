6be0fdc6a4141ff0f7a09f2d20e95c94fb35b8b9
taketdev <maxleyh17@proton.me>
2025-03-14 14:05:02 +0100

    added dishes objects

---
+++ scripts/dishes.js
@@ -0,0 +1,106 @@
+const dishes = [
+    // Nudelgerichte
+    {
+      id: 1,
+      category: "nudelgerichte",
+      name: "Pad Thai",
+      description: "Gebratene Reisnudeln mit Tofu, Ei, Sojasprossen, Erdnüssen und Limette",
+      price: 11.90,
+      image: "./assets/img/food_img/Nudelgerichte/padthai.jpg"
+    },
+    {
+      id: 2,
+      category: "nudelgerichte",
+      name: "Yakisoba",
+      description: "Japanische gebratene Nudeln mit knackigem Gemüse und Hühnchen",
+      price: 12.90,
+      image: "./assets/img/food_img/Nudelgerichte/Yakisoba.jpg"
+    },
+    {
+      id: 3,
+      category: "nudelgerichte",
+      name: "Ramen",
+      description: "Japanische Nudelsuppe mit reichhaltiger Brühe, Chashu und Frühlingszwiebeln",
+      price: 13.50,
+      image: "./assets/img/food_img/Nudelgerichte/rmane.jpg"
+    },
+    
+    // Curry
+    {
+      id: 4,
+      category: "curry",
+      name: "Grünes Thai Curry",
+      description: "Scharfes grünes Curry mit Kokosmilch, Gemüse und Jasminreis",
+      price: 14.90,
+      image: "./assets/img/food_img/Curry/grünescurry.jpg"
+    },
+    {
+      id: 5,
+      category: "curry",
+      name: "Rotes Curry mit Ente",
+      description: "Aromatisches rotes Curry mit zarter Ente, Litschi und Gemüse",
+      price: 16.90,
+      image: "./assets/img/food_img/Curry/rotesthaicurry.jpg"
+    },
+    {
+      id: 6,
+      category: "curry",
+      name: "Massaman Curry",
+      description: "Mildes cremiges Curry mit Kartoffeln, Erdnüssen und Rindfleisch",
+      price: 15.50,
+      image: "./assets/img/food_img/Curry/massaman.jpg"
+    },
+    
+    // Spezialitäten
+    {
+      id: 7,
+      category: "spezialitaeten",
+      name: "Dumplings",
+      description: "Gedämpfte Teigtaschen gefüllt mit Schweinefleisch und Gemüse",
+      price: 9.90,
+      image: "./assets/img/food_img/Spezialitäten/dumblings.jpg"
+    },
+    {
+      id: 8,
+      category: "spezialitaeten",
+      name: "Bibimbap",
+      description: "Koreanische Reisschüssel mit Gemüse, Ei und Rindfleisch",
+      price: 14.50,
+      image: "./assets/img/food_img/Spezialitäten/bibimbap.jpg"
+    },
+    {
+      id: 9,
+      category: "spezialitaeten",
+      name: "Peking Ente",
+      description: "Knusprige Ente mit Pfannkuchen, Frühlingszwiebeln und Hoisin-Sauce",
+      price: 18.90,
+      image: "./assets/img/food_img/Spezialitäten/pekingente.jpg"
+    }
+  ];
+  
+  const drinks = [
+    {
+      id: 10,
+      category: "getraenke",
+      name: "Fanta",
+      price: 2.50,
+      image: "./assets/img/food_img/Getränke/fanta.jpg"
+    },
+    {
+      id: 11,
+      category: "getraenke",
+      name: "Sprite",
+      price: 2.50,
+      image: "./assets/img/food_img/Getränke/sprite.jpg"
+    },
+    {
+      id: 12,
+      category: "getraenke",
+      name: "Coca Cola",
+      price: 2.50,
+      image: "./assets/img/food_img/Getränke/cola.jpg"
+    }
+  ];
+  
+  export { dishes, drinks };
+  
